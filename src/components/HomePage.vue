085a65c8d336ff436f5960b85411c283a84617f6
Thomas Clifford <tclifford5225@gmail.com>
2022-03-03 00:15:29 -0500

    Added Links and Headers to Page

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="hello">
+  <div id="page-container">
     <div>
       <h2>Search Committee Resources</h2>
       <p>The following are a working list of suggested resources compiled by the Division of Diverse and<br>
       Inclusive Excellence for publicizing available openings at KSU.</p>
     </div>
 
-    <a target="_blank" :href="link"><button v-on:click="getHref">Click Me!</button></a>
-    <!-- <a v-bind:href="a.href" :key="index" v-bind:class="[link]" v-for="(a, index) in getHref">{{ a.link }}</a> -->
-    <!-- <form class="searchform" action="action_page.php">
-      <input type="text" placeholder="Search.." name="search" class="searchbar">
-      <button type="submit" class="searchbtn">Search</button>
-    </form> -->
+    <div id="main-container">
+      <div v-for="(section, index) in sections" :key="index">
+        <h3>{{section.header}}</h3>
+        <div class="long">
+          <a v-for="(link, name, index) in section.links" target="_blank" :href="link" :key="index">{{name}}</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,26 +23,72 @@ export default {
   name: 'HomePage',
   data() {
     return {
-      link: "https://www.awaplusd.org/"
+      links: [],
+      architectureConstruction: [],
+      arts: [],
+      financeBusiness: [],
+      disabilitiesProtectedveterans: [],
+      informationTechnology: [],
+      regionalResources: [],
+      mathScience: [],
+      nursing: [],
+      studentAffairs: [],
+      sections: []
+      
     }
   },
+  mounted() {
+    this.getHrefs()
+  },
   methods: {
-    async getHref () {
-      await LINKS.get().then(querySnapshot => 
+    async getHrefs () {
+      var sectionsOfWebpage = [];
+      var linksFromDatabase = [];
+      var docIDs = [];
+      await LINKS.get().then(querySnapshot => {
         querySnapshot.forEach(doc => {
-          if (doc.id === "Architecture_Construction") {
-            const data = doc.data();
-            console.log(data.testField);
-            return data.textField;
-          }
+          const data = doc.data();
+          docIDs.push(doc.id);
+          linksFromDatabase.push(data);
         })
-      );
-    }
+      })
+      this.links = linksFromDatabase;
+      for (var i = 0; i < this.links.length; i++) {
+        sectionsOfWebpage.push({
+          header: docIDs[i],
+          links: this.links[i]
+        })
+      }
+      console.log(sectionsOfWebpage);
+      this.sections = sectionsOfWebpage;
+    },
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#main-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
 
+  grid-template-rows: 1fr 1fr 1fr;
+  gap: 20px;
+  height: calc(100vh - 10px);
+}
+a {
+    display: block;
+    padding-bottom: 5px;
+    text-decoration: none;
+}
+
+a:hover{
+  color: pink;
+}
+
+.long {
+  /* max-height: 500px; */
+  overflow-y: scroll;
+  height: 30%;
+}
 </style>
